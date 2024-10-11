8665730f790fe39567128b7555f2d3e215e7dfcf
Oluwalolope <134228462+Oluwalolope@users.noreply.github.com>
2024-10-11 03:01:35 +0100

    Complete responsive primary navigation styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,63 +1,49 @@
-import {useRef} from 'react';
-import menuOpen from "../assets/icon-menu.svg";
-import menuClose from "../assets/icon-close.svg";
-import logo from "../assets/logo.svg";
+import { useRef } from 'react';
+import menuOpen from '../assets/icon-menu.svg';
+import menuClose from '../assets/icon-close.svg';
+import logo from '../assets/logo.svg';
 import Cart from './Cart';
 import Avatar from './Avatar';
 
 const NavBar = () => {
-    const navRef = useRef();
+  const navRef = useRef();
 
-    const handleShowNavbar = () => {
-        navRef.current.classList.toggle('responsive_nav');
-    }
+  const handleShowNavbar = () => {
+    navRef.current.classList.toggle('opened');
+  };
 
-    return (
-      <>
-        <div className="logo" aria-label="Company Logo">
+  return (
+    <>
+      <div className="logo">
+        <button
+          onClick={handleShowNavbar}
+          aria-controls="primary-navigation"
+          className="nav-btn nav-btn-open"
+          aria-expanded="false"
+        >
+          <span className="visually-hidden">Menu</span>
+          <img src={menuOpen} alt="" aria-hidden="true" />
+        </button>
+
+        <a href="#" aria-label="Company Logo">
+          <img src={logo} alt="Company Logo" />
+        </a>
+      </div>
+
+      <div className="nav-wrapper">
+        <nav
+          ref={navRef}
+          className="primary-navigation"
+          id="primary-navigation"
+        >
           <button
             onClick={handleShowNavbar}
-            aria-label="Menu button"
-            aria-expanded="false"
+            aria-controls="primary-navigation"
+            className="nav-btn nav-btn-close"
+            aria-expanded="true"
           >
-            <img src={menuOpen} alt="icon for opening menu" />
-          </button>
-
-
-          <img src={logo} alt="Company Logo" />
-        </div>
-        <nav ref={navRef}>
-          <ul role="list" className="desktop-visible">
-            <li>
-              <a href="#">Collections</a>
-            </li>
-            <li>
-              <a href="#">Men</a>
-            </li>
-            <li>
-              <a href="#">Women</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-
-          <ul role="list">
-            <li>
-              <Cart />
-            </li>
-            <li>
-              <Avatar />
-            </li>
-          </ul>
-        </nav>
-
-        <nav className="mobile-visible">
-          <button onClick={handleShowNavbar}>
-            <img src={menuClose} alt="icon for closing menu" />
+            <span className="visually-hidden">Menu</span>
+            <img src={menuClose} alt="" aria-hidden="true" />
           </button>
 
           <ul role="list">
@@ -78,8 +64,14 @@ const NavBar = () => {
             </li>
           </ul>
         </nav>
-      </>
-    );
-}
+
+        <menu>
+          <Cart />
+          <Avatar />
+        </menu>
+      </div>
+    </>
+  );
+};
 
 export default NavBar;
